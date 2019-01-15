e3762016c3d81859051d4ae60abf23ede7d33cb6
zzzSeMzzz <zzzSeMzzz@bk.ru>
2019-01-15 19:39:58 +0300

    fix btn_minus bag. Add keystore

--- app/src/main/java/sem/ru/plusminus/mvp/view/MainView.java
+++ app/src/main/java/sem/ru/plusminus/mvp/view/MainView.java
@@ -1,15 +1,12 @@
 package sem.ru.plusminus.mvp.view;
 
-import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy;
-import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
-
 import java.util.List;
 
 import sem.ru.plusminus.mvp.model.Event;
 
 public interface MainView extends BaseView {
 
-    @StateStrategyType(OneExecutionStateStrategy.class)
+    //@StateStrategyType(OneExecutionStateStrategy.class)
     void setItems(List<Event> events);
 
     void showEmptyText(boolean show);
